65ec5ab462223c991bae560462aa44289d3a37ba
Benny Chopra <bchopra@bsd220.org>
2024-11-21 09:41:26 -0600

    add dev to github

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -2,20 +2,78 @@
 //  ContentView.swift
 //  Tic Tac Toe
 //
-//  Created by Benny Chopra on 11/21/24.
+//  Created by Benny Chopra on 11/18/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "",count: 9)
+    @State private var xTurn = true
+    @State private var gameOver = false
+    @State private var winMessage = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tic Tac Toe")
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3), content: {
+                ForEach(0..<9) { index in
+                    ZStack{
+                        Color.blue
+                        Color.white
+                            .opacity(moves[index] == "" ? 1 : 0)
+                        Text(moves[index])
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120,alignment: .center)
+                    .cornerRadius(30)
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            if moves[index] == "" {
+                                moves[index] = xTurn ? "X" : "0"
+                                xTurn.toggle()
+                            }
+                        }
+                    }
+                    .rotation3DEffect(
+                        .degrees(moves[index] == "" ? 180 : 0), axis: (x: 0.0, y: 1.0, z: 0.0)
+                    )
+                }
+            })
         }
-        .padding()
+        .preferredColorScheme(.dark)
+        .alert(isPresented: $gameOver) {
+            Alert(title: Text(winMessage), dismissButton: .destructive(Text("Play Again"),
+                                                                       action: {
+                withAnimation{
+                    moves = Array(repeating: "", count: 9)
+                    gameOver = false
+                }
+            }))
+        }
+        .onChange(of: moves) { oldValue, newValue in checkForWinner()
+        }
+    }
+    private func checkForWinner() {
+        checkLine(a: 0, b: 1, c: 2)//top row
+        checkLine(a: 3, b: 4, c: 5)//middle row
+        checkLine(a: 6, b: 7, c: 8)//bottom row
+        checkLine(a: 2, b: 5, c: 8)//right row
+        checkLine(a: 1, b: 4, c: 7)//middle up-down row
+        checkLine(a: 0, b: 3, c: 6)//left row
+        checkLine(a: 0, b: 4, c: 8)//diagnol left
+        checkLine(a: 2, b: 4, c: 6)//diagnol right
+        
+        if !(gameOver || moves.contains("")){
+            winMessage = "Cat's Game"
+            gameOver = true
+        }
+    }
+    private func checkLine(a: Int, b: Int, c: Int) {
+        if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
+            winMessage = "\(moves[a]) is the winner!"
+            gameOver = true
+        }
     }
 }
 
